c4db9e7256e77adc4958cd027676beff4ad6e684
Ana Júlia <meloanajulia30@gmail.com>
2023-08-29 13:44:50 -0300

    up

--- js/midia-social.js
+++ js/midia-social.js
@@ -12,6 +12,8 @@ const messageSearch = document.querySelector('#message-search');
 /* TEMA */
 const theme = document.querySelector('#theme');
 const themeModal = document.querySelector('.customize_tema');
+const fontSize = document.querySelectorAll('.choose-size');
+var root = document.querySelector(':root');
 
 
 /// -----SIDEBAR-----
@@ -84,3 +86,45 @@ themeModal.addEventListener('click', closeThemeModal);
 
 
 theme.addEventListener('click', openThemeModal);
+
+
+/* FONTES */
+
+fontSize.forEach(size => {
+    let fontSize;
+
+    size.addEventListener('click', () => {
+        if(size.classList.contains('font-size-1')) {
+            fontSize = '10px';
+            root.style.setProperty('--sticky-top-left', '5.4rem');
+            root.style.setProperty('----sticky-top-right', '5.4rem');
+        } else if(size.classList.contains('font-size-2')) {
+            fontSize = '13px';
+            root.style.setProperty('--sticky-top-left', '5.4rem');
+            root.style.setProperty('--sticky-top-right', '-7rem');
+        } else if(size.classList.contains('font-size-3')) {
+            fontSize = '16px';
+            root.style.setProperty('--sticky-top-left', '-2rem');
+            root.style.setProperty('--sticky-top-right', '-17rem');
+        } else if(size.classList.contains('font-size-4')) {
+            fontSize = '19px';
+            root.style.setProperty('--sticky-top-left', '-5rem');
+            root.style.setProperty('--sticky-top-right', '-25rem');
+        } else if(size.classList.contains('font-size-5')) {
+            fontSize = '22px';
+            root.style.setProperty('--sticky-top-left', '-10rem');
+            root.style.setProperty('--sticky-top-right', '-33rem');
+        }
+    /* mudar a fonte */
+    document.querySelector('html').style.fontSize = fontSize;
+    })
+    
+
+})
+
+
+
+
+
+
+
